a258859d8391406f03da8a4a911e1d0ad9439952
Michał Wiśniewski <michu123qwe@gmail.com>
2019-09-09 10:56:54 +0200

    added raw index view

--- housechores/tasks/views.py
+++ housechores/tasks/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Task
+
 
 # Create your views here.
+class IndexView(generic.ListView):
+    template_name = 'tasks/index.html'
+
+    def get_queryset(self):
+        return Task.objects.order_by('-pub_date')
