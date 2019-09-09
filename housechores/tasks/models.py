86905f0628b27339106c7c8ecbb8f3c31955fd73
Michał Wiśniewski <michu123qwe@gmail.com>
2019-09-09 10:29:57 +0200

    database and model setup

--- housechores/tasks/models.py
+++ housechores/tasks/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class Task(models.Model):
+    caption = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date added')
+    due_date = models.DateTimeField('due date')
+    task_giver = models.CharField(max_length=50)
+    task_done_by = models.CharField(max_length=50, blank=True)
+    task_done_date = models.DateTimeField('done date', blank=True, null=True)
+
+    def __str__(self):
+        return '{} by {}'.format(self.caption, self.task_giver)
