cdd6746f05fc571189178cc995f01b80477d678a
Michał Wiśniewski <michu123qwe@gmail.com>
2019-09-10 09:35:47 +0200

    added raw implementation of completing tasks

--- housechores/tasks/urls.py
+++ housechores/tasks/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'tasks'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('<int:task_id>/complete_task/', views.complete_task, name='complete_task'),
 ]
--- housechores/tasks/views.py
+++ housechores/tasks/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Task
+from django.utils import timezone
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 
 # Create your views here.
@@ -9,3 +12,15 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         return Task.objects.order_by('-pub_date')
+
+
+def complete_task(request, task_id):
+    task = get_object_or_404(Task, pk=task_id)
+    if task.task_done_by or not request.user.is_authenticated():
+        return HttpResponseRedirect(reverse('tasks:index'))
+
+    task.task_done_by = request.user.username
+    task.task_done_date = timezone.now()
+    task.save()
+
+    return HttpResponseRedirect(reverse('tasks:index'))
