bb695de21a57822e314f0cbbee4ade5e8031eccf
Michał Wiśniewski <michu123qwe@gmail.com>
2019-09-13 11:48:46 +0200

    changed max_length of caption, task_giver and task_done_by to 30

--- housechores/tasks/models.py
+++ housechores/tasks/models.py
@@ -4,11 +4,11 @@ from django.utils import timezone
 
 # Create your models here.
 class Task(models.Model):
-    caption = models.CharField(max_length=200)
+    caption = models.CharField(max_length=30)
     pub_date = models.DateTimeField('date added')
     due_date = models.DateTimeField('due date')
-    task_giver = models.CharField(max_length=50)
-    task_done_by = models.CharField(max_length=50, blank=True)
+    task_giver = models.CharField(max_length=30)
+    task_done_by = models.CharField(max_length=30, blank=True)
     task_done_date = models.DateTimeField('done date', blank=True, null=True)
 
     def __str__(self):
