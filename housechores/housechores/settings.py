55c47c1ab9c011392696157d3125f2e7413a13a1
Michał Wiśniewski <michu123qwe@gmail.com>
2019-09-15 11:08:35 +0200

    added 0.0.0.0 to allowed hosts

--- housechores/housechores/settings.py
+++ housechores/housechores/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9670ei*332zgujw(-v^t3^p!#&&a$8**pwa=)asv*qks1&-l2o'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
